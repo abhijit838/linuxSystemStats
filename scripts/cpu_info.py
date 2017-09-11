eeea869e4463a47aa00bef22184c7c6d923cbf0a
abhijit838 <abhijit.maity2010@gmail.com>
2017-09-12 01:56:38 +0530

    Added cpu info

--- scripts/cpu_info.py
+++ scripts/cpu_info.py
@@ -1,4 +1,24 @@
 # Provides cpu info
 
-def get_cpu_info():
-    info = list()
+from collections import OrderedDict
+
+def get_cpuinfo():
+    cpuinfo = {}
+    procinfo = {}
+
+    nprocs = 0
+    with open('/proc/cpuinfo') as f:
+        for line in f:
+            if not line.strip():
+                cpuinfo['proc%s' % nprocs] = procinfo
+                nprocs += 1
+                procinfo = {}
+            else:
+                if len(line.split(':')) == 2:
+                    procinfo[line.split(':')[0].strip()] = line.split(':')[1].strip()
+                else:
+                    procinfo[line.split(':')[0].strip()] = ''
+
+    return cpuinfo
+if __name__ == '__main__':
+    print(get_cpuinfo())
